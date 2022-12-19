dc4f850394e5b9b849df4b91d47438f75265319e
Exora02 <antoine.leyvraz@gmail.com>
2022-12-19 18:51:10 +0100

    update de la structure. ajout de la camera dynamique qui suit un certain objet et possibilité de faire bouger la boule au clavier

---
+++ env.py
@@ -0,0 +1,26 @@
+import pymunk as pm
+import pymunk.pygame_util
+
+class Env:
+    def __init__(self, window) -> None:
+        self.window = window
+        self.space = pm.Space()
+        self.space.gravity =(0, -981)
+        pymunk.pygame_util.positive_y_is_up = True
+        self.createFloor()
+    
+    def createFloor(self):
+        body = pm.Body(body_type=pm.Body.STATIC)
+        body.position = (self.window.get_width()/2, 20)
+        shape = pm.Poly.create_box(body, (100, 20))
+        self.space.add(body, shape)
+        
+    def addObject(self):
+        body = pm.Body()
+        body.position = (self.window.get_width()/2, 60)
+        shape = pm.Circle(body, 20)
+        shape.mass = 10
+        self.space.add(body,shape)
+        
+    def step(self,dt):
+        self.space.step(dt)
--- main.py
+++ main.py
@@ -2,44 +2,54 @@ import pymunk as pm
 import pymunk.pygame_util
 import pygame as pg
 import math
+from env import *
 
 
 class App:
-    def __init__(self, env) -> None:
+    def __init__(self) -> None:
         self.WIDTH = 980
         self.HEIGHT = 720
         self.fps = 60
         self.dt = 1/self.fps
         self.window = pg.display.set_mode((self.WIDTH, self.HEIGHT))
         self.physicsWindow = self.window.subsurface(pg.Rect(self.WIDTH/4,self.HEIGHT/20,self.WIDTH/2,self.HEIGHT/2))
+        self.cameraStart = None
+        self.shapeToFollow = None
         self.cameraOffset = pg.Vector2((0,0))
         self.draw_options = pymunk.pygame_util.DrawOptions(self.physicsWindow)
         self.running = True
-        self.env = env
+        self.env = Env(self.physicsWindow)
 
-    def addObject(self):
-        body = pm.Body()
-        body.position = (self.physicsWindow.get_width()/2, self.physicsWindow.get_height() - 100)
-        shape = pm.Circle(body, 20)
-        shape.mass = 10
-        self.env.space.add(body,shape)
+    
 
     def eventHandler(self, events):
         for event in events:
             if event.type == pg.QUIT:
                 self.running = False
                 break
+            if event.type == pg.KEYDOWN:
+                pressed = pg.key.get_pressed()
+                if pressed[pg.K_LEFT]: self.env.space._shapes[2].body.apply_force_at_local_point((-10000,0), (0,0))
+                if pressed[pg.K_RIGHT]: self.env.space._shapes[2].body.apply_force_at_local_point((10000,0), (0,0))
             
     def updateCamera(self):
-        pressed = pygame.key.get_pressed()
-        camera_move = pygame.Vector2()
-        if pressed[pygame.K_UP]: camera_move += (0, 1)
-        if pressed[pygame.K_LEFT]: camera_move += (1, 0)
-        if pressed[pygame.K_DOWN]: camera_move += (0, -1)
-        if pressed[pygame.K_RIGHT]: camera_move += (-1, 0)
+        if not self.cameraStart:
+            self.cameraStart = self.env.space._shapes[2].body.position
+        if not self.shapeToFollow:
+            self.shapeToFollow = self.env.space._shapes[2]
+        mainBodyPos = self.shapeToFollow.body.position
+        self.cameraOffset = pm.Vec2d(-(mainBodyPos.x - self.cameraStart.x), -(mainBodyPos.y - self.cameraStart.y))
+        print(self.cameraOffset)
+        """
+        pressed = pg.key.get_pressed()
+        camera_move = pg.Vector2()
+        if pressed[pg.K_UP]: camera_move += (0, 1)
+        if pressed[pg.K_LEFT]: camera_move += (1, 0)
+        if pressed[pg.K_DOWN]: camera_move += (0, -1)
+        if pressed[pg.K_RIGHT]: camera_move += (-1, 0)
         if camera_move.length() > 0: camera_move.normalize_ip()
-        camera += camera_move*(dt/5)
-            
+        self.cameraOffset += camera_move
+        """    
 
     def draw(self):
         self.window.fill(pg.Color(210, 218, 226))
@@ -48,20 +58,11 @@ class App:
         self.draw_options.transform = pm.Transform.translation(self.cameraOffset.x,self.cameraOffset.y)
         self.env.space.debug_draw(self.draw_options)
         pg.display.update()
-        
-        
-    def createFloor(self):
-        body = pm.Body(body_type=pm.Body.STATIC)
-        body.position = (self.physicsWindow.get_width()/2, 20)
-        shape = pm.Poly.create_box(body, (self.WIDTH, 20))
-        self.env.space.add(body, shape)
-        #body.angle += 0.2
-        #self.env.space.reindex_shape(shape)
-    
+
+  
     def run(self):
         clock = pg.time.Clock()
-        self.createFloor()
-        self.addObject()
+        self.env.addObject()
         while self.running:
             self.eventHandler(pg.event.get())
 
@@ -72,22 +73,10 @@ class App:
         pg.quit()
 
 
-class Env:
-    def __init__(self) -> None:
-        self.space = pm.Space()
-        self.space.gravity =(0, -981)
-        pymunk.pygame_util.positive_y_is_up = True
-        
-    def step(self,dt):
-        self.space.step(dt)
-
-
 def setup():
     pg.init()
-    env = Env()
-    app = App(env)
+    app = App()
     app.run()
-
 
 
 if __name__ == "__main__":
